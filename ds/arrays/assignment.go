deafb479165c5fce9de4f626fe0ce0feaeeed6a2
Amit Kumar <amitkannaujiyanitk@gmail.com>
2024-11-06 22:53:33 +0530

    formatting

--- ds/arrays/assignment.go
+++ ds/arrays/assignment.go
@@ -45,7 +45,6 @@ func pivotIndex(nums []int) int {
 	return -1
 }
 
-
 func missingNumberMethod1(nums []int) int {
 	sum, n := 0, len(nums)
 	// calculate sum
@@ -53,10 +52,11 @@ func missingNumberMethod1(nums []int) int {
 		sum += num
 	}
 	// now missing no will be expected total sum - current sum
-	// expected sum for [0,n] will be 
+	// expected sum for [0,n] will be
 	// expected sum = (n * (n + 1))/2
 	return ((n * (n + 1)) / 2) - sum
 }
+
 // store occurance of no in array
 func missingNumberMethod2(nums []int) int {
 	countArray := make([]int, len(nums))
@@ -73,26 +73,27 @@ func missingNumberMethod2(nums []int) int {
 	// for safe return
 	return 0
 }
+
 // cyclic sort : execution of below array
 // 2, 3, 1, 5, 0 swap 2 with 1
 // 1, 3, 2, 5, 0 swap 1 with 3
 // 3, 1, 2, 5, 0 swap 3 with 5
 // 5, 1, 2, 3, 0 swap 5 with 0
 // 0, 1, 2, 3, 5 now every element is at correct position
-// increament i from 0 to 5 
+// increament i from 0 to 5
 func missingNumberMethod3(nums []int) int {
 	i := 0
 	for i < len(nums) {
 		// here range of nums is [0, n] , so correct position i if array was sorted will be equal to the nums[i]
 		correctPosition := nums[i]
-		if nums[i] != nums[correctPosition] {  // swap num with correct postion no if it is not matching
+		if nums[i] != nums[correctPosition] { // swap num with correct postion no if it is not matching
 			nums[i], nums[correctPosition] = nums[correctPosition], nums[i]
 		} else {
-			i++   // num is already at correct postion , check next element , so incrementing
+			i++ // num is already at correct postion , check next element , so incrementing
 		}
 	}
 	// missing number will the one who's index will not match with the number
-	for i:=0; i < len(nums); i++ {
+	for i := 0; i < len(nums); i++ {
 		if nums[i] != i {
 			return i
 		}
@@ -101,61 +102,61 @@ func missingNumberMethod3(nums []int) int {
 	return 0
 }
 
-// remove duplicated 
+// remove duplicated
 func removeDuplicates(nums []int) int {
-    // correct index, should start from zero
-    correctIndex := 0
-    // loop will start from 1
-    for i :=1; i < len(nums); i++ {
-        // if no is not duplicate
-        if nums[i] != nums[correctIndex] {
-            // then place it at 
-            nums[correctIndex + 1] = nums[i]
-            correctIndex++
-        }
-    }
-    return correctIndex + 1
+	// correct index, should start from zero
+	correctIndex := 0
+	// loop will start from 1
+	for i := 1; i < len(nums); i++ {
+		// if no is not duplicate
+		if nums[i] != nums[correctIndex] {
+			// then place it at
+			nums[correctIndex+1] = nums[i]
+			correctIndex++
+		}
+	}
+	return correctIndex + 1
 }
 
 // Sliding window approach to find maximum
 func findMaxAverage(nums []int, k int) float64 {
-    max := -12121122211.656 // minFloat not available in lib so used some dummy valye
-    start, sum := 0, 0
-    for end:=0; end < len(nums); end++ {
-        // add element in window
-        sum += nums[end]
-        if end - start + 1 == k {
-            // calculate average when window is reached
-            avrage := float64(sum) / float64(k)
-            // update max
-            if max < avrage {
-                max = avrage
-            }
-            // remove element from window
-            sum -= nums[start]
-            start++
-        }
-    }
-    return max
+	max := -12121122211.656 // minFloat not available in lib so used some dummy valye
+	start, sum := 0, 0
+	for end := 0; end < len(nums); end++ {
+		// add element in window
+		sum += nums[end]
+		if end-start+1 == k {
+			// calculate average when window is reached
+			avrage := float64(sum) / float64(k)
+			// update max
+			if max < avrage {
+				max = avrage
+			}
+			// remove element from window
+			sum -= nums[start]
+			start++
+		}
+	}
+	return max
 }
 
 func sortColors(nums []int) {
 	start, mid, end := 0, 0, len(nums)-1
-    // <= is needed when mid is at 1 by swaping 2 and loops end with some pending operations
-    // try for [2, 0, 1] case
+	// <= is needed when mid is at 1 by swaping 2 and loops end with some pending operations
+	// try for [2, 0, 1] case
 	for mid <= end {
-        // if mid is at 0 , swap it with start as start points to 0
+		// if mid is at 0 , swap it with start as start points to 0
 		if nums[mid] == 0 {
 			nums[mid], nums[start] = nums[start], nums[mid]
 			mid++
 			start++
-            // if mid is at 1, it is at correct place 
+			// if mid is at 1, it is at correct place
 		} else if nums[mid] == 1 {
 			mid++
 		} else {
-            // mid is at 2, swap it with end and just decrease end 
+			// mid is at 2, swap it with end and just decrease end
 			nums[mid], nums[end] = nums[end], nums[mid]
-			end-- // mid is not incremented as it is similar to binary sort and end is at right side 
+			end-- // mid is not incremented as it is similar to binary sort and end is at right side
 		}
 	}
 }
@@ -163,22 +164,22 @@ func sortColors(nums []int) {
 // copy positive first, then copy negative
 func segregateElements(nums []int) {
 	copy := make([]int, len(nums))
-    p := 0
+	p := 0
 	for i := 0; i < len(nums); i++ {
-        if nums[i] > 0 {
-            copy[p] = nums[i]
-            p++
-        }
+		if nums[i] > 0 {
+			copy[p] = nums[i]
+			p++
+		}
 	}
-    for i := 0; i < len(nums); i++ {
-        if nums[i] < 0 {
-            copy[p] = nums[i]
-            p++
-        }
+	for i := 0; i < len(nums); i++ {
+		if nums[i] < 0 {
+			copy[p] = nums[i]
+			p++
+		}
 	}
-    for i := 0; i < len(nums); i++ {
-        nums[i] = copy[i]
-    }
+	for i := 0; i < len(nums); i++ {
+		nums[i] = copy[i]
+	}
 }
 
 func findDuplicate(nums []int) int {
@@ -193,6 +194,7 @@ func findDuplicate(nums []int) int {
 	// return for safe exit
 	return -1
 }
+
 // cyclic sort : execution of below array
 // 2, 3, 1, 5, 0 swap 2 with 1
 // 1, 3, 2, 5, 0 swap 1 with 3
@@ -211,28 +213,28 @@ func findDuplicateCyclicSort(nums []int) int {
 			i++ // move to next index
 		}
 	}
-	return nums[n-1] //  as duplicate no will move to end 
+	return nums[n-1] //  as duplicate no will move to end
 }
 
 func findMissingAndRepeatingElement(nums []int) []int {
-	ans := make([]int , 2)
+	ans := make([]int, 2)
 
-	for i:=0; i < len(nums); i++ {
-		index := utill.ABS(nums[i]) // math.abs is used because 
+	for i := 0; i < len(nums); i++ {
+		index := utill.ABS(nums[i]) // math.abs is used because
 		// mark the element as visited , convert it to negative
-		if nums[index - 1] > 0 {
-			nums[index - 1] *= -1
+		if nums[index-1] > 0 {
+			nums[index-1] *= -1
 		} else {
-			// if it is already negative means it is seen previously 
+			// if it is already negative means it is seen previously
 			// this will be repeated element
 			ans = append(ans, index)
 		}
 	}
-	for i:=0; i < len(nums); i++ {
+	for i := 0; i < len(nums); i++ {
 		if nums[i] > 0 {
-			// If some element is positive means 
+			// If some element is positive means
 			// that index is missing
-			ans = append(ans, i + 1)
+			ans = append(ans, i+1)
 		}
 	}
 	return ans
@@ -245,7 +247,7 @@ func firstRepeated(nums []int) int {
 	}
 	for i, num := range nums {
 		if freqMap[num] > 1 {
-			return i + 1 // 1 based index 
+			return i + 1 // 1 based index
 		}
 	}
 	return -1
@@ -258,7 +260,7 @@ func possibleSums(coins []int, quants []int) int {
 	return len(sumMap)
 }
 
-func possibleSumRec(coins []int, quants []int, index , sum int, sumMap map[int]struct{}) {
+func possibleSumRec(coins []int, quants []int, index, sum int, sumMap map[int]struct{}) {
 	// base case
 
 	if index == len(coins) {
@@ -267,9 +269,9 @@ func possibleSumRec(coins []int, quants []int, index , sum int, sumMap map[int]s
 	}
 
 	// 1 case
-	for i:=0; i <= quants[index]; i++ {
-		newSum := sum + coins[index] * i
-		possibleSumRec(coins, quants, index + 1, newSum, sumMap)
+	for i := 0; i <= quants[index]; i++ {
+		newSum := sum + coins[index]*i
+		possibleSumRec(coins, quants, index+1, newSum, sumMap)
 	}
 
 	// recursion
--- ds/arrays/assignment_test.go
+++ ds/arrays/assignment_test.go
@@ -1,10 +1,10 @@
 package arrays
 
 import (
-	"testing"
 	"github.com/stretchr/testify/assert"
+	"testing"
 )
 
 func TestPossibleSumRec(t *testing.T) {
-	assert.Equal(t, 9, possibleSums([]int{10, 50, 100}, []int{1,2,1}))
+	assert.Equal(t, 9, possibleSums([]int{10, 50, 100}, []int{1, 2, 1}))
 }
